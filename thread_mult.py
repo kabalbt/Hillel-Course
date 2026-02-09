81bcf92a9e47a83b5a2596ef221b460ad4a0c278
kabalbt <Kabalbt@gmail.com>
2026-02-09 19:36:24 +0100

    HW9 thread_&_multip lucky ticket

--- thread_mult.py
+++ thread_mult.py
@@ -1,30 +1,42 @@
+import datetime
 import logging
-import threading
-import time
-
-def is_lucky_number(ticket_number: str):
-    if len(ticket_number) != 6 or not ticket_number.isdigit():
-        return "invalid ticket number"
-
-    left = sum(map(int, ticket_number[:3]))
-    right = sum(map(int, ticket_number[3:]))
-
-    if left == right:
-        print(f"{ticket_number} is lucky number")
-    else:
-        print(f"{ticket_number} is common number")
+from multiprocessing import Process, Manager
 
 
+
+
+def is_lucky_number(start_range, end_range, result_list):
+   cnt = 0
+   for i in range(start_range, end_range):
+       ticket = f"{i:06d}"
+       if sum(map(int, ticket[:3])) == sum(map(int, ticket[3:])):
+           cnt = cnt + 1
+   result_list.append(cnt)
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        level=logging.INFO
-    )
+    manager = Manager()
+    results = manager.list()
 
-logging.info("Main : before creating thread")
-x = threading.Thread(target=is_lucky_number, args=("123042",))
-logging.info("Main : after running thread")
-x.start()
-x.join()
-logging.info("Main : all done")
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    logging.info("Main : before creating Process")
+    x1 = Process(target=is_lucky_number, args=(0, 250000, results))
+    x2 = Process(target=is_lucky_number, args=(250000, 500000, results))
+    x3 = Process(target=is_lucky_number, args=(500000, 750000, results))
+    x4 = Process(target=is_lucky_number, args=(750000, 1000000, results))
+    logging.info("Main : after running thread")
+    t1 = datetime.datetime.now()
+
+    for p in (x1, x2, x3, x4):
+        p.start()
+
+    logging.info("Main : wait for the thread to finish")
+    for p in (x1, x2, x3, x4):
+        p.join()
+
+    t2 = datetime.datetime.now()
+    logging.info("Main : all done")
+    logging.info(f"total time spent: {t2 - t1}")
+
+    print("TOTAL:", sum(results))
